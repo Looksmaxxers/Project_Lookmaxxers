2a67e9c67f87655a2b4988c306da272314565d81
Isildur512 <78391349+Isildur512@users.noreply.github.com>
2024-02-13 13:20:14 -0500

    Added UI for healing items

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -2,22 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CharacterStats : MonoBehaviour
 {
     [SerializeField] private float curHealth;
     [SerializeField] private float maxHealth;
-    [SerializeField] public Scrollbar HSlider;
+    [SerializeField] private int flaskNum;
+    [SerializeField] private Scrollbar HSlider;
+    [SerializeField] private TMP_Text FlaskNumDisplay;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        flaskNum = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
         HSlider.size = (curHealth / maxHealth);
+        FlaskNumDisplay.text = flaskNum.ToString();
     }
 }
