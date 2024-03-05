cc7b785391e39d22485ca20ae2a8a324566eb51c
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2024-03-04 22:14:15 -0500

    Added attack and weapon script

---
+++ Assets/WeaponScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    private int[] hitEnemies; // Array to keep track of enemies hit by the weapon
+
+    public string againstTag;
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitEnemies = new int[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (
+                other.gameObject.tag == againstTag && 
+                ArrayUtility.IndexOf(hitEnemies, other.gameObject.GetInstanceID()) != -1
+            )
+        {
+            hitEnemies.Append(other.gameObject.GetInstanceID());
+            IEntityStats stats = other.gameObject.GetComponent<IEntityStats>();
+            stats.TakeDamage(damage);
+        }
+    }
+
+    public void OnAttackEnd()
+    {
+        hitEnemies = new int[0];
+    }
+}
