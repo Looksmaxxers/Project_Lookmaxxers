97ce48fbd1dd26850f76e32c98fae082548ffa46
mikewang <wangzhxn0@gmail.com>
2024-03-11 04:03:00 -0400

    Stamina readition

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -9,25 +9,34 @@ public class CharacterStats : MonoBehaviour, IEntityStats
 {
     [SerializeField] private float curHealth;
     [SerializeField] private float maxHealth;
+    [SerializeField] private float curStamina;
+    [SerializeField] private float maxStamina;
     [SerializeField] private int flaskNum;
     [SerializeField] private int maxFlasks;
     [SerializeField] private Scrollbar HSlider;
+    [SerializeField] private Scrollbar SSlider;
     [SerializeField] private TMP_Text FlaskNumDisplay;
     [SerializeField] private GameObject curBonfire;
     [SerializeField] private float staggerThreshold;
+    [SerializeField] private float staminaRecoverScalar;
 
     private Animator anim;
     private CharacterController cController;
     private ThirdPersonController tController;
     private WeaponScript weaponScript;
-    
+    private StarterAssetsInputs sai;
+
 
     public bool isRolling = false;
     public bool isAttacking = false;
     public bool isStaggered = false;
     public bool isDead = false;
+    public bool isSprinting = false;
     public float defaultStaggerThreshold = 8f;
     public GameObject weaponRoot;
+    public float cooldownTimer = 0.0f;
+    public float recoverScalar = 20.0f;
+
 
     public GameObject vfxSlashObj;
     public GameObject slashRoot;
@@ -47,18 +56,28 @@ public class CharacterStats : MonoBehaviour, IEntityStats
         cController = GetComponent<CharacterController>();
         tController = GetComponent<ThirdPersonController>();
         weaponScript = weaponRoot.GetComponent<WeaponScript>();
+        sai = GetComponent<StarterAssetsInputs>();
         staggerThreshold = defaultStaggerThreshold;
+    }
+    public bool spendStamina(float spentValue)
+    {
+        float netStamina = curStamina - spentValue;
+        curStamina = netStamina >= 0 ? netStamina : curStamina;
+        return netStamina >= 0 ? true : false;
     }
 
     // Update is called once per frame
     void Update()
     {
         HSlider.size = (curHealth / maxHealth);
+        SSlider.size = (curStamina / maxStamina);
+
         FlaskNumDisplay.text = flaskNum.ToString();
         staggerThreshold = Mathf.Max(defaultStaggerThreshold, staggerThreshold - 1f * Time.deltaTime);
 
         isRolling = anim.GetBool("Roll");
         isAttacking = anim.GetBool("Attack");
+        isSprinting = sai.sprint;
         //isStaggered = anim.GetBool("Stagger");
 
         if (isDead)
@@ -100,12 +119,41 @@ public class CharacterStats : MonoBehaviour, IEntityStats
             Heal();
         }
 
+        recoverStamina();
+
+        if (isSprinting && !isRolling)
+        {
+            if (curStamina >= 0)
+            {
+                curStamina -= (3.0f * Time.deltaTime);
+            }
+        }
+
 
         // if (!isRolling && !isStaggered && !isAttacking && Input.GetKeyDown(KeyCode.Mouse0))
         // {
         //     anim.SetBool("Attack", true);
         // }
 
+    }
+    public void recoverStamina()
+    {
+        if (isRolling || isAttacking || isSprinting)
+        {
+            cooldownTimer = 2;
+        }
+        else if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+
+        if (cooldownTimer <= 0)
+        {
+            if (curStamina < maxStamina)
+            {
+                curStamina += (recoverScalar * Time.deltaTime);
+            }
+        }
     }
 
     public void TakeDamage(int damage)
